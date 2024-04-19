d63bae3f86b7b4a32cf0c857cb537c9e4933c02a
Nick Stuart <nmstuart20@gmail.com>
2024-04-19 18:52:22 -0500

    add database query

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use actix_web::web::get;
 use std::net::TcpListener;
-
-use zero2prod::run;
+use zero2prod::configuration::get_configuration;
+use zero2prod::{configuration, startup::*};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let address = TcpListener::bind("127.0.0.1:8000")?;
-    run(address)?.await
+    let configuration = get_configuration().expect("Failed to read configuration file");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
