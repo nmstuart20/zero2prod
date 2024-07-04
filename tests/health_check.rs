2e8788506a1661b4b4dcfb4aa88ea386e1d464b2
nickyflash <nmstuart20@gmail.com>
2024-07-04 16:58:13 -0500

    finished chapter 6

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -135,3 +135,32 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
         );
     }
 }
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_fields_are_present_but_empty() {
+    let test_app = spawn_app().await;
+
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+        ("name=Ursula&email=", "empty email"),
+        ("name=Ursual&email=definitely-not-an-email", "invalid email"),
+    ];
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &test_app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The response did not return a 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
+}
